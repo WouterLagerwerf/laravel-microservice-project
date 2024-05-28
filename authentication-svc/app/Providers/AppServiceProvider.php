80cf6a69c2381f434172b8bbd7f73aca250cb107
WouterLagerwerf <43637154+WouterLagerwerf@users.noreply.github.com>
2024-05-28 17:09:11 +0200

    Redone the authentication for Passport
    Enabling password clients so we can have Oauth2 support per workspace on user level

--- authentication-svc/app/Providers/AppServiceProvider.php
+++ authentication-svc/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Passport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +12,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        Passport::ignoreRoutes();
+        // get all the functions on the Passport facade
+        Passport::enablePasswordGrant();
+        Passport::tokensExpireIn(now()->addDays(15));
+        Passport::refreshTokensExpireIn(now()->addDays(30));
+        Passport::personalAccessTokensExpireIn(now()->addMonths(6));
     }
 
     /**
--- authentication-svc/routes/api.php
+++ authentication-svc/routes/api.php
@@ -4,6 +4,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\ValidateWorkspace;
 use App\Http\Controllers\UsersController;
+use Laravel\Passport\Http\Controllers\AccessTokenController;
+use Laravel\Passport\Http\Controllers\TransientTokenController;
+use Laravel\Passport\Http\Controllers\AuthorizationController;
+use Laravel\Passport\Http\Controllers\ApproveAuthorizationController;
+use Laravel\Passport\Http\Controllers\DenyAuthorizationController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -14,13 +19,11 @@ Route::middleware([ValidateWorkspace::class])->group(function () {
         Route::apiResource('users', UsersController::class);
         Route::post('users/{id}/roles', [UsersController::class, 'addRole']);
         Route::delete('users/{id}/roles', [UsersController::class, 'removeRole']);
+         // redefining the passport routes we need
+        Route::post('/oauth/token', [AccessTokenController::class, 'issueToken']);
+        Route::post('/oauth/token/refresh', [TransientTokenController::class, 'refresh']);
+        Route::get('/oauth/authorize', [AuthorizationController::class, 'authorize']);
+        Route::post('/oauth/authorize', [ApproveAuthorizationController::class, 'approve']);
+        Route::delete('/oauth/authorize', [DenyAuthorizationController::class, 'deny']);
     });
-});
-
-Route::get('testing', function () {
-    $job = new \App\Jobs\Workspaces\CreateWorkspaceEvent([
-        'endpoint' => 'asfasfasfasfqwqeqeqqqqqwefsft',
-    ]);
-
-    $job->handle(new \App\Services\DbService(), new \App\Services\ClientService());
 });
